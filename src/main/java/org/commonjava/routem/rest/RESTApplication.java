5ab2d268ddd348f827b444bb4b015f95366d01a8
John Casey <jdcasey@commonjava.org>
2011-11-29 17:00:05 -0500

    adding application definition and installer listener

---
+++ src/main/java/org/commonjava/routem/rest/RESTApplication.java
@@ -0,0 +1,11 @@
+package org.commonjava.routem.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath( "/api/1.0" )
+public class RESTApplication
+    extends Application
+{
+
+}
